5124e076eae147971a295e7a50c2e264d9235815
alejandbel <bogdanov28c@gmail.com>
2023-08-28 18:57:31 +0300

    Add project files.

---
+++ WebLab/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		public IActionResult Lab2()
+		{
+			return View();
+		}
+	}
+}
